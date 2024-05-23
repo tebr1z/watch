ddd1cac7793020ef29df6ae6ca358de91fd611c5
Tabriz <126268845+tebr1z@users.noreply.github.com>
2024-05-24 01:29:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const min = document.querySelector("#minute");
 const hours = document.querySelector("#hour");
 const deg = 6; //6 derce: 1 satin tam bir kruqu 360 dercecedi 360 / 60 = 6
 function watch() {
-  let hour = new Date().getHours() * 30;
-  let minute = new Date().getMinutes() * deg;
-  let second = new Date().getSeconds() * deg;
+  let hour = new Date().getHours() * 30; //12 satliq`da her saat 30 derce doner (360/12)
+  let minute = new Date().getMinutes() * deg; // deqiqede 6 derece doner
+  let second = new Date().getSeconds() * deg; // saniyede 6 derece doner
 
-  hours.style.transform = `rotate(${hour + minute / 12}deg)`; //12 satliq`da her saat 30 derce doner (360/12)
+  hours.style.transform = `rotate(${hour + minute / 12}deg)`;  
   min.style.transform = `rotate(${minute}deg)`; // deqiqede 6 derece doner
   sec.style.transform = `rotate(${second}deg)`;// saniyede 6 derece doner
 }
